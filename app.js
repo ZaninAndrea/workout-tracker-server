e2fb2d80d5e2bb0bd7ce173f830c913bd120a7e3
Zanin Andrea <andrea@igloo.ooo>
2019-01-12 19:38:05 +0100

    minimal routes

--- app.js
+++ app.js
@@ -1,10 +1,35 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const { insertWorkout, findAllWorkouts } = require("./database")
 
 const app = express()
 app.use(bodyParser.json())
 
-app.get("/", (req, res) => [res.send("homepage")])
+app.get("/", (req, res) => {
+    res.send("homepage")
+})
+
+app.post("/workout", async (req, res) => {
+    const data = req.body
+    try {
+        const workout = await insertWorkout(data)
+
+        res.send(workout.insertedId)
+    } catch (e) {
+        res.statusCode(500).send("Internal error")
+    }
+})
+
+app.get("/workouts/:date", async (req, res) => {
+    const date = req.params.date
+    try {
+        const workouts = await findAllWorkouts({ date })
+
+        res.send(workouts)
+    } catch (e) {
+        res.statusCode(500).send("Internal error")
+    }
+})
 
 const port = 3000
 app.listen(port, () => console.log("listening on port 3000"))
