1cf935c292f6bc9aad3ce7dc86f76a278d7f742d
Zanin Andrea <andrea@igloo.ooo>
2019-01-12 20:00:33 +0100

    cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const cors = require("cors")
 const {
     insertWorkout,
     findAllWorkouts,
@@ -9,6 +10,7 @@ const {
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send("homepage")
